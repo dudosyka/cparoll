a973e7c6d4e99b3ca978a5390ae97cb865408bdf
dudosyka <45633581+dudosyka@users.noreply.github.com>
2022-04-24 18:39:11 +0300

    Arrow

--- main.js
+++ main.js
@@ -41,46 +41,75 @@ const changeBlock = function (type) {
     }
 }
 
+let currSection = 1;
+
+const changeLabel = function (img) {
+    console.log(img)
+    document.getElementById("label").style.opacity = "0";
+    setTimeout(() => {
+        for (let key in document.getElementById('label').children) {
+            if (document.getElementById('label').children[key]['style'])
+                document.getElementById('label').children[key].style.display = 'none';
+        }
+        document.getElementById(img).style.display = 'block';
+        document.getElementById("label").style.opacity = "1";
+    }, 150)
+}
+
 const animate = function (scrollPos) {
-    console.log()
-    if (scrollPos >= document.getElementById('block2Content_').offsetTop - window.innerHeight) {
-        document.getElementById('block2Content').style.left = "250px";
-        if (currentBlock2 == 1)
-            document.getElementById('block2Content').style.opacity = '1'
-        else
-            document.getElementById('block2Content_').style.opacity = '1'
-        document.getElementById('block2Content_').style.left = "-180px";
+    if (scrollPos >= document.getElementById('block4Content').offsetTop - window.innerHeight) {
+        document.getElementById('block4Content').style.left = "0px";
+        setTimeout(() => {
+            document.getElementById('block4Content').style.opacity = '1'
+        }, 200)
+        if (currSection != 4)
+            changeLabel( "cu")
+        currSection = 4
+        return
     }
-    else if (scrollPos < document.getElementById('block2Content').offsetTop - window.innerHeight) {
-        document.getElementById('block2Content').style.left = "-1000px";
-        document.getElementById('block2Content').style.opacity = '0'
-        document.getElementById('block2Content_').style.left = "-1000px";
-        document.getElementById('block2Content_').style.opacity = '0'
+    else {
+        document.getElementById('block4Content').style.left = "-1000px";
+        document.getElementById('block4Content').style.opacity = '0'
     }
     if (scrollPos >= document.getElementById('block3Content').offsetTop - window.innerHeight) {
         document.getElementById('block3Content').style.left = "0px";
         setTimeout(() => {
             document.getElementById('block3Content').style.opacity = '1'
         }, 200)
+        if (currSection != 3)
+            changeLabel( "a")
+        currSection = 3
+        return
     }
     else {
         document.getElementById('block3Content').style.left = "-1000px";
         document.getElementById('block3Content').style.opacity = '0'
     }
-    if (scrollPos >= document.getElementById('block4Content').offsetTop - window.innerHeight) {
-        document.getElementById('block4Content').style.left = "0px";
-        setTimeout(() => {
-            document.getElementById('block4Content').style.opacity = '1'
-        }, 200)
+    if (scrollPos >= document.getElementById('block2Content_').offsetTop - window.innerHeight && scrollPos <= document.getElementById('block2Content_').offsetTop + 100) {
+        document.getElementById('block2Content').style.left = "250px";
+        if (currentBlock2 == 1)
+            document.getElementById('block2Content').style.opacity = '1'
+        else
+            document.getElementById('block2Content_').style.opacity = '1'
+        document.getElementById('block2Content_').style.left = "-180px";
+        if (currSection != 2)
+            changeLabel( "p")
+        currSection = 2
+        return
     }
-    else {
-        document.getElementById('block4Content').style.left = "-1000px";
-        document.getElementById('block4Content').style.opacity = '0'
+    else if (scrollPos < document.getElementById('block2Content').offsetTop - window.innerHeight) {
+        document.getElementById('block2Content').style.left = "-1000px";
+        document.getElementById('block2Content').style.opacity = '0'
+        document.getElementById('block2Content_').style.left = "-1000px";
+        document.getElementById('block2Content_').style.opacity = '0'
+        if (currSection != 1)
+            changeLabel( "sd")
+        currSection = 1;
+        return
     }
+    return;
 }
 
 window.addEventListener('scroll', function(e) {
-    console.log(e)
-    console.log(window.scrollY)
     animate(window.scrollY)
 });
